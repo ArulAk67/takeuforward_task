d3b01a520be5e32be352c042e88d93ba9ba9345e
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2024-03-21 10:00:55 +0530

    table ui

--- frontend/src/TableView.js
+++ frontend/src/TableView.js
@@ -20,31 +20,32 @@ function TableView() {
 
     fetchData();
   }, []);
-
+  // console.log(tableData);
   return (
-    <div>
-      <h2>Table View</h2>
-      <table>
-        <thead>
-          <tr>
-            {tableData.length > 0 &&
-              Object.keys(tableData[0]).map((key) => (
-                <th key={key}>{key}</th>
+    <div className="mx-auto p-4 w-full bg-black text-emerald-500 min-h-screen flex flex-col items-center">
+      <div>
+      <h2 className="font-bold mb-4 text-center italic text-3xl">Table View</h2>
+      </div>
+      <div className="overflow-x-auto font-medium">
+        <table className=" table-auto w-full">
+          <thead>
+            <tr>
+              {tableData.length > 0 && Object.keys(tableData[0]).map((key, index) => (
+                <th key={index} className="border px-16 py-8 capitalize italic text-xl">{key}</th>
               ))}
-            {/* Generate table headers based on the keys of the first object in tableData */}
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((row, index) => (
-            <tr key={index}>
-              {Object.values(row).map((value, idx) => (
-                <td key={idx}>{value}</td>
-              ))}
-              {/* Generate table cells based on the values of each row */}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {tableData.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {Object.values(row).map((value, colIndex) => (
+                  <td key={colIndex} className="border px-8 py-4 text-xl">{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
